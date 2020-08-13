efd8b8a45cf118c47579621d8c2685a0b27b12b3
cerny.matej <cerny.matej@gmail.com>
2020-08-13 19:10:07 +0200

    GeneratorTest simplified

--- src/test/scala/common/GeneratorTest.scala
+++ src/test/scala/common/GeneratorTest.scala
@@ -4,28 +4,18 @@ import common.Domain.{Amount, Currency}
 import common.Generator._
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
-import org.scalatest.funsuite.AnyFunSuite
-
-class GeneratorTest extends AnyFunSuite {
-
-  test("randomPerson") {
-    val p = randomPerson.asString.split(" ")
-
-    assert(p.size == 2)
-    assert(names.contains(p.head))
-    assert(surnames.contains(p(1)))
-  }
-
-  test("randomCurrency") {
-    assert(Currency.toList.contains(randomCurrency))
-  }
-
-}
 
 object GeneratorTest extends Properties("common.GeneratorTest") {
 
   property("randomAmount") = forAll { _: Int =>
     randomAmount.value >= Amount.min.value && randomAmount.value <= Amount.max.value
   }
+
+  property("randomPerson") = {
+    val p = randomPerson.asString.split(" ")
+    p.size == 2 && names.contains(p.head) && surnames.contains(p(1))
+  }
+
+  property("randomCurrency") = Currency.toList.contains(randomCurrency)
 
 }
